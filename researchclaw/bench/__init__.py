5973a210558ece5d660bd32a715cdd8ed8b5296c
MikaelCool <96781663+MikaelCool@users.noreply.github.com>
2026-06-11 11:35:23 +0800

    add bench package exports

---
+++ researchclaw/bench/__init__.py
@@ -0,0 +1,5 @@
+"""ResearchClaw benchmark primitives."""
+
+from researchclaw.bench.core import BenchmarkMetric, BenchmarkReport, BenchmarkResult, BenchmarkTask
+
+__all__ = ["BenchmarkMetric", "BenchmarkReport", "BenchmarkResult", "BenchmarkTask"]
